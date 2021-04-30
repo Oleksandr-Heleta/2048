9d38823ad2a1010b9276175a938372f179d3676f
Oleksandr-Heleta <sanock.geleta@gmail.com>
2021-04-30 23:53:20 +0300

    some fixed

--- models/matrixModel.js
+++ models/matrixModel.js
@@ -15,40 +15,39 @@ class MatrixModel extends BaseModel {
         }
         if (!MatrixModel.instance) {
             MatrixModel.instance = this
-          };
-          
+        };
+
         return MatrixModel.instance;
-    
+
     }
-    
+
     startNewGame() {
         this.attributes.grid = [
-                            ['', '', '', ''],
-                            ['', '', '', ''],
-                            ['', '', '', ''],
-                            ['', '', '', ''],
-                        ]
-        randomGrid(this.attributes.grid, this.attributes.size.height,  this.attributes.size.width);
-        randomGrid(this.attributes.grid, this.attributes.size.height,  this.attributes.size.width);
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+            ['', '', '', ''],
+        ]
+        randomGrid(this.attributes.grid, this.attributes.size.height, this.attributes.size.width);
+        randomGrid(this.attributes.grid, this.attributes.size.height, this.attributes.size.width);
         this.publish('changeData');
     }
 
     makeActionByKey(key) {
+        let cloneArr = this.attributes.grid;
         if (key === 'left' || key === 'right') {
             this.attributes.grid = moveHorizont(this.attributes.grid, key);
         } else {
             let turnGrids = rowInColum(this.attributes.grid);
-            if(key === 'up'){
+            if (key === 'up') {
                 turnGrids = moveHorizont(turnGrids, 'left');
-            } else{
+            } else {
                 turnGrids = moveHorizont(turnGrids, 'right');
             }
             this.attributes.grid = rowInColum(turnGrids);
         }
-        // if (this.attributes.grid.indexOf('') < 0) {
-        //     alert('Press NEW GAME')
-        // }
-        randomGrid(this.attributes.grid, this.attributes.size.height,  this.attributes.size.width);
+        if (similarArr(cloneArr, this.attributes.grid)) return;
+        randomGrid(this.attributes.grid, this.attributes.size.height, this.attributes.size.width);
         this.publish('changeData');
     }
 };
--- utils/util.js
+++ utils/util.js
@@ -1,6 +1,6 @@
 function templateStr(tpl, attributes) {
-    for(var i in attributes) {
-        if(attributes.hasOwnProperty(i)) {
+    for (var i in attributes) {
+        if (attributes.hasOwnProperty(i)) {
             tpl = tpl.replace('{{' + i + '}}', attributes[i]);
         }
     }
@@ -11,32 +11,32 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function randomGrid(grids, height, width){
-    let j = getRandom(0, height-1);
-    let i = getRandom(0, width-1);
+function randomGrid(grids, height, width) {
+    let j = getRandom(0, height - 1);
+    let i = getRandom(0, width - 1);
     if (grids[j][i] === '') {
         grids[j][i] = '2';
         return grids; // Доделать рандом 2/4
-    } 
-    return randomGrid(grids,height, width);
+    }
+    return randomGrid(grids, height, width);
 };
 
 function fullArr(arr) {
     const stack = [];
-    for(let char of arr) {
+    for (let char of arr) {
         if (char !== '') findChar(char);
     }
-    function findChar(char){
-        if(stack.length > 0 && stack[stack.length-1].char === char) {
-            stack[stack.length-1].appeared++;
-            if(stack[stack.length-1].appeared === 2) {
+    function findChar(char) {
+        if (stack.length > 0 && stack[stack.length - 1].char === char) {
+            stack[stack.length - 1].appeared++;
+            if (stack[stack.length - 1].appeared === 2) {
                 stack.pop();
-                char = String( +char * 2);
+                char = String(+char * 2);
                 findChar(char);
-                
+
             }
         } else {
-            stack.push({char, appeared: 1});
+            stack.push({ char, appeared: 1 });
         }
     }
     let res = [];
@@ -44,14 +44,25 @@ function fullArr(arr) {
         res.push(n.char);
     })
     return res;
-    };
+};
 
 function moveHorizont(grids, move) {
     let res = grids.map(j => {
-        let numberArr = fullArr(j);
+        let numberArr = [];
+        switch (move) {
+            case 'left':
+                numberArr = fullArr(j);
+                break;
+            case 'right':
+                numberArr = fullArr(j.reverse()).reverse();
+                break;
+            default:
+                return;
+        }
+
         let diffLength = j.length - numberArr.length;
         let emptyArr = [];
-        for (i = 0; i < diffLength; i += 1){
+        for (i = 0; i < diffLength; i += 1) {
             emptyArr.push('');
         }
         switch (move) {
@@ -61,24 +72,35 @@ function moveHorizont(grids, move) {
                 return j = emptyArr.concat(numberArr);
             default:
                 return;
-            }
+        }
     })
     return res;
 };
 
 function rowInColum(grids) {
     let turnArr = [];
-    for(j = 0; j < grids.length; j += 1){
+    for (j = 0; j < grids.length; j += 1) {
         turnArr.push([]);
-        for(i = 0; i < grids[j].length; i += 1){
+        for (i = 0; i < grids[j].length; i += 1) {
             turnArr[j].push('');
         }
     }
-    
-    for(j = 0; j < grids.length; j += 1){
-        for(i = 0; i < grids.length; i += 1){
+
+    for (j = 0; j < grids.length; j += 1) {
+        for (i = 0; i < grids.length; i += 1) {
             turnArr[i][j] = grids[j][i];
         }
     }
     return turnArr;
+};
+
+function similarArr(firstArr, secondArr) {
+    if (firstArr.length !== secondArr.length) return false;
+    for (j = 0; j < firstArr.length; j += 1) {
+        if (firstArr[j].length !== secondArr[j].length) return false;
+        for (i = 0; i < firstArr[j].length; i += 1) {
+            if (firstArr[j][i] !== secondArr[j][i]) return false;
+        }
+    }
+    return true;
 }
